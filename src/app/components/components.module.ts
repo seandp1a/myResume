f34b4b33d84fe0dec8b9275fa99937ed4610fd58
sean <seanchang@systex.com>
2021-09-01 16:17:33 +0800

    angular material progress bar animation

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,19 +1,23 @@
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArticleCardComponent } from './article-card/article-card.component';
-
-
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [ArticleCardComponent],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule
   ],
-  exports:[
+  exports: [
     CommonModule,
-    ArticleCardComponent
+    ArticleCardComponent,
+    MatProgressBarModule,
+    MatProgressSpinnerModule
   ]
 })
 export class ComponentsModule { }
